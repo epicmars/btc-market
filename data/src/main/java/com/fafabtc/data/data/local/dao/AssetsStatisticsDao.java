6bdd906eade60a127b89ac943363528386a9a1b6
jastrelax <yinpinjiu@gmail.com>
2018-04-22 19:32:59 +0800

    Fix sqlite3 version compatibility when using 'with' clause

--- data/src/main/java/com/fafabtc/data/data/local/dao/AssetsStatisticsDao.java
+++ data/src/main/java/com/fafabtc/data/data/local/dao/AssetsStatisticsDao.java
@@ -14,29 +14,32 @@ import java.util.List;
 @Dao
 public interface AssetsStatisticsDao {
 
-    @Query("with \n" +
-            "     all_quotes(q) as (select distinct quote from ticker),\n" +
-            "     quote_ticker as (select * from ticker where base in all_quotes and quote = 'usdt' COLLATE NOCASE),\n" +
-            "     assets as (select blockchain_assets.assets_uuid,\n" +
-            "     blockchain_assets.exchange,\n" +
-            "     blockchain_assets.name,\n" +
-            "     blockchain_assets.available,\n" +
-            "     blockchain_assets.locked,\n" +
-            "     ticker.last,\n" +
-            "     ticker.\"quote\",\n" +
-            "     ticker.base_volume\n" +
-            "     from blockchain_assets left outer join ticker\n" +
-            "     on blockchain_assets.name = ticker.base\n" +
-            "     and blockchain_assets.exchange = ticker.exchange\n" +
-            "     where blockchain_assets.assets_uuid = :assetsUUID),\n" +
-            "     statistics as (select assets.*, quote_ticker.last as quote_last,\n" +
-            "        case\n" +
-            "            when assets.\"quote\" = 'usdt' COLLATE NOCASE then assets.last\n" +
-            "            else assets.last * quote_ticker.last\n" +
-            "        end\n" +
-            "        as usdt_last\n" +
-            "     from assets left outer join quote_ticker on assets.\"quote\" = quote_ticker.base)\n" +
-            "     select *, avg(statistics.usdt_last) as usdt_avg, max(base_volume) as max_base_volume from statistics group by statistics.name, statistics.exchange;")
+    @Query("select *, avg(statistics.usdt_last) as usdt_avg, max(base_volume) as max_base_volume \n" +
+           "from (select assets.*, quote_ticker.last as quote_last, \n" +
+           "          case\n" +
+           "              when assets.quote = 'usdt' COLLATE NOCASE \n" +
+           "                  then assets.last \n" +
+           "              else assets.last * quote_ticker.last \n" +
+           "          end \n" +
+           "          as usdt_last \n" +
+           "      from (select blockchain_assets.assets_uuid, \n" +
+           "            blockchain_assets.exchange, \n" +
+           "            blockchain_assets.name, \n" +
+           "            blockchain_assets.available, \n" +
+           "            blockchain_assets.locked, \n" +
+           "            ticker.last, \n" +
+           "            ticker.quote, \n" +
+           "            ticker.base_volume \n" +
+           "            from blockchain_assets left outer join ticker\n" +
+           "            on blockchain_assets.name = ticker.base \n" +
+           "            and blockchain_assets.exchange = ticker.exchange\n" +
+           "      where blockchain_assets.assets_uuid = :assetsUUID) as assets \n" +
+           "      left outer join \n" +
+           "          (select * from ticker where base in \n" +
+           "              (select distinct quote from ticker) \n" +
+           "           and quote = 'usdt' COLLATE NOCASE) as quote_ticker \n" +
+           "      on assets.quote = quote_ticker.base) as statistics \n" +
+           "group by statistics.name, statistics.exchange;")
     @SuppressWarnings(RoomWarnings.CURSOR_MISMATCH)
     List<AssetsStatistics> findAssetsStatistics(String assetsUUID);
 }
